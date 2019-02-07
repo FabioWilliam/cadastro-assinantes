00b6c75e3754c0a5e4b4da2457367898de6ad67f
Fabio William <fabiowilliam@rafanne.com.br>
2019-02-07 21:22:58 -0200

    adicao da mascara telefone

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/inputmask@4.0.6/dist/jquery.inputmask.bundle.js"></script>
+    <script>
+
+        $(document).ready(function() {
+            $('#cpf').inputmask('999.999.999-99');
+            $('#data_nascimento').inputmask('99/99/9999');
+            $('#cep').inputmask('99999-999');
+            $('#telefone').inputmask('(99) 99999-9999');
+        });
+
+    </script>
     <style>
 
         body {
