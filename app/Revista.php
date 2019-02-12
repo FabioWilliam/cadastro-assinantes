4edce6e3d6f7801d81b4938ce7d21db2814ece34
Fabio William <fabiowilliam@rafanne.com.br>
2019-02-12 18:55:43 -0200

    finalizacao da pagina index de revistas

--- app/Revista.php
+++ app/Revista.php
@@ -16,4 +16,11 @@ class Revista extends Model
         return explode(',', $value);
     }
 
+    public function getDescricaoReduzida()
+    {
+        $descricao = explode(' ', $this->descricao);
+        $descricao_simples = $descricao[0] . ' ' . $descricao[1] . ' ' . $descricao[2] . ' ' . $descricao[3];
+        return $descricao_simples;
+    }
+
 }
--- resources/views/revista/index.blade.php
+++ resources/views/revista/index.blade.php
@@ -12,37 +12,39 @@
     <div class="actions py-2">
         <a href="{{ route('revistas.create') }}" class="btn btn-primary">Criar Revista</a>
     </div>
-    <table class="table table-striped">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th>Código</th>
-                <th>Título</th>
-                <th>Descrição</th>
-                <th>Valor</th>
-                <th>Vigência</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($revistas as $revista)
-            <tr>
-                <td>{{ $revista->codigo }}</td>
-                <td>{{ $revista->titulo }}</td>
-                <td>{{ $revista->descricao }}</td>
-                <td>{{ $revista->valor }}</td>
-                <td>{{ $revista->vigencia }}</td>
-                <td>
-                    <a href="{{ route('revistas.show', ['id' => $revista->id]) }}" class="btn btn-outline-primary btn-sm">visualizar</a>
-                    <a href="{{ route('revistas.edit', ['id' => $revista->id]) }}" class="btn btn-outline-primary btn-sm">editar</a>
-                    <form action="{{ route('revistas.destroy', ['id' => $revista->id]) }}" method="POST" style="display: inline">
-                        @method('DELETE')
-                        @csrf
-                        <button type="submit" class="btn btn-outline-primary btn-sm" onclick="javascript: return confirm('Você deseja realmente apagar este revista?')">remover</button>
-                    </form>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
+    <div class="table-responsive-lg">
+        <table class="table table-striped table-sm">
+            <thead>
+                <tr>
+                    <th scope="col">Cód.</th>
+                    <th scope="col">Título</th>
+                    <th scope="col">Descrição</th>
+                    <th scope="col">Valor</th>
+                    <th scope="col">Vigência</th>
+                    <th scope="col">Ações</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($revistas as $revista)
+                <tr>
+                    <th scope="row">{{ $revista->codigo }}</td>
+                    <td>{{ $revista->titulo }}</td>
+                    <td>{{ $revista->getDescricaoReduzida() }}</td>
+                    <td>{{ $revista->valor }}</td>
+                    <td>{{ $revista->vigencia }}</td>
+                    <td>
+                        <a href="{{ route('revistas.show', ['id' => $revista->id]) }}" class="btn btn-outline-primary btn-sm">visualizar</a>
+                        <a href="{{ route('revistas.edit', ['id' => $revista->id]) }}" class="btn btn-outline-primary btn-sm">editar</a>
+                        <form action="{{ route('revistas.destroy', ['id' => $revista->id]) }}" method="POST" style="display: inline">
+                            @method('DELETE')
+                            @csrf
+                            <button type="submit" class="btn btn-outline-primary btn-sm" onclick="javascript: return confirm('Você deseja realmente apagar este revista?')">remover</button>
+                        </form>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
     {{ $revistas->links() }}
 @endsection
